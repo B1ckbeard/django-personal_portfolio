423b679a1fcd7de77aac65d1f0085923b1ba8d7f
BlackBeard <evs.diman@yandex.ru>
2021-10-07 17:10:36 +0300

    3 commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from portfolio.models import Project
+from django.template import loader
 
 def home(request):
-    data = Project.objects.values()
-    for i in data:
-        print(i)
-
-
-    return render(request, 'portfolio/home.html')
+    #s=''
+    #for project in Project.objects.all():
+    #    s+=project.title
+    template =loader.get_template('portfolio/home.html')
+    objects = Project.objects.all()
+    context = {'objects': objects}
+    return HttpResponse(template.render(context, request))
