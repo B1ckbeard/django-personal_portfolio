2c61721ed22b6e1124dfa347f83612e18d448fc0
Black_Beard <evs.diman@yandex.ru>
2021-11-14 18:22:07 +0300

    17 comm

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,10 +5,6 @@ def home(request):
     projects = Project.objects.all()
     return render(request,'portfolio/home.html', {'projects': projects} )
 
-def detail(request, project_id):
-    project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'portfolio/detail.html', {'project':project})
-
 def portfolio(request):
     projects = Project.objects.all()
     images = Image.objects.all()
